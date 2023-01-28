294078ef42fdb67c9a987e44dc6ae2ad07587487
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-28 14:10:54 -0300

    jkifile

--- aws-projeto/securitygp.tf
+++ aws-projeto/securitygp.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "sg-networking" {
   ingress {
     from_port   = "3306"
     to_port     = "3306"
-    protocol    = "mysql"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
