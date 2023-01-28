3142d2b0e1008adbdad9a546bb92d43f2005c747
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-28 14:25:35 -0300

    tffile

--- aws-projeto/securitygp.tf
+++ aws-projeto/securitygp.tf
@@ -97,14 +97,14 @@ resource "aws_security_group" "jkins-networking" {
     from_port   = "8081"
     to_port     = "8081"
     protocol    = "tcp"
-    cidr_blocks = ["187.74.168.177/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
     ingress {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = ["187.74.168.177/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
