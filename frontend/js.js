bd3ff3baf54206cd2421c8de6b50ba0ac5c2501e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-28 21:14:57 -0300

    file

--- frontend/js.js
+++ frontend/js.js
@@ -4,7 +4,7 @@ $("#btn_salvar").on("click", function() {
     var txt_mensagem = $("#mensagem").val(); 
 
     $.ajax({
-        url: "http://endpointurl/",
+        url: "http://a64d0f8a4ea5b4642a45b096b6915122-431721300.us-east-1.elb.amazonaws.com/",
         type: "post",
         data: {nome: txt_nome, mensagem: txt_mensagem},
         beforeSend: function() {
